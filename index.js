20827396d72068dadeb32e43f118a8d48040a73c
Marius59 <marius.hammer@smail.th-koeln.de>
2018-12-27 17:47:46 +0100

    Router und Fetcher laufen ... halbwegs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Importiert Pakete
 var express = require ('express');
-var router = require('router');
+var router = require('./router');
 var app = express();
+var fetcher = require('./fetcher');
+var fetchES6 = require('isomorphic-fetch');
+require('es6-promise').polyfill();
 
 
-
-
+app.use('/',router);
 
 
 
--- router.js
+++ router.js
@@ -2,12 +2,23 @@
 // Init
 var express = require('express');
 var router = express.Router();
+var fetcher = require('./fetcher');
 
 // Routing
 
 
 router.get('/',function (req,res,next) {
+
     res.send("Hey BUUUUUU");
+});
+
+
+
+router.get('/:geo1/:geo2',function (req,res,next) {
+    var geo1 = req.param("geo1");
+    var geo2 = req.param("geo2");
+    fetcher.getInfo(geo1,geo2);
+    res.send(geo1 + geo2);
 });
 
 
